19d457c30edbf4de3da88d75adbf5bdc0fdd3686
223yu <223yuta.jp@gmail.com>
2022-06-19 21:31:47 +0900

    #6 confirm response

--- server.rb
+++ server.rb
@@ -1,4 +1,5 @@
 require 'webrick'
+
 srv = WEBrick::HTTPServer.new({
   :DocumentRoot => './',
   :BindAddress => '127.0.0.1',
@@ -9,7 +10,7 @@ srv.mount('/', WEBrick::HTTPServlet::FileHandler, 'index.html')
 
 srv.mount_proc '/test' do |req, res|
   test = req.request_line
-  puts test
+  res.body = 'test_response'
 end
 
 trap("INT"){ srv.shutdown }
